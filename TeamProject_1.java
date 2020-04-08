59366c58251bcc455201b3f5e2a4d35c970b59ac
lejk8104 <lejk8104@gmail.com>
2020-04-08 23:16:40 +0900

    change a code

--- TeamProject_1.java
+++ TeamProject_1.java
@@ -8,16 +8,16 @@ import java.util.InputMismatchException;
  */
 public class TeamProject_1
 {
+    static double speed;
     public static void main(String[] args) {
-        String signal;                                                                              //변수선언1 Signal
-        double speed;                                                                               //변수선언2 Speed
-        Scanner scan = new Scanner(System.in);                                                      //Scanner Package scan 변수 선언
+        String signal;
+        Scanner scan = new Scanner(System.in);
         while (true) {
             System.out.println("---------------------------------");
             System.out.println("Select traffic light from green, yellow, or red:");
             signal = scan.next();
-            if (!(signal.equals("green") || signal.equals("yellow") || signal.equals("red"))) {     //만약 입력받은 신호가 초록,노랑,빨강이 아니라면
-                System.out.println("Select from green, yellow, red. Type again");                   //재입력
+            if (!(signal.equals("green") || signal.equals("yellow") || signal.equals("red"))) {
+                System.out.println("Select from green, yellow, red. Type again");
                 continue;
             }
             
@@ -29,27 +29,72 @@ public class TeamProject_1
                 scan.next();
                 continue;
             }
-            if (speed <= 0) {                                                                   //예외처리: 입력받은 스피드가 0보다 작다면
+            if (speed <= 0) {
+                System.out.println("Please set the robot's speed as positive to make it move");
+                continue;import java.util.Scanner;
+import java.util.InputMismatchException;
+/**
+ * This class is for controling the speed of the robot depending on the colors of a traffic light
+ * 
+ * @author (우메모토세이야_2018315056, 이재근_2015225129) 
+ * @version (2020-04-03)
+ */
+public class TeamProject_1
+{
+    static double speed;
+    public static void main(String[] args) {
+        String signal;
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("---------------------------------");
+            System.out.println("Select traffic light from green, yellow, or red:");
+            signal = scan.next();
+            if (!(signal.equals("green") || signal.equals("yellow") || signal.equals("red"))) {
+                System.out.println("Select from green, yellow, red. Type again");
+                continue;
+            }
+            
+            System.out.println("Select your robot's speed:");
+            try {
+                speed = scan.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Please input a number");
+                scan.next();
+                continue;
+            }
+            if (speed <= 0) {
                 System.out.println("Please set the robot's speed as positive to make it move");
                 continue;
             }
             
-            speed = RobotWalk(signal,speed);                                                    //
+            RobotWalk(signal, speed);
             System.out.println("traffic light:" + signal + ", speed:" + speed);
-            continue;
         }
     }
-    public static double RobotWalk(String signal, double speed) {
-        if (signal.equals("green")) {       //조건1: 교통신호가 녹색이라면
-            return speed;                   //속도를 변경하지 않고 보행
+    public static void RobotWalk(String signal, double spd) {
+        if (signal.equals("green")) { 
         } else {
-            if (signal.equals("yellow")) {  //조건2: 교통신호가 노란색이라면
-                speed = speed*1.2;
-                return speed;                //속도를 증가하여 보행
-            } else {                        //조건3: 교통신호가 빨간색이라면
+            if (signal.equals("yellow")) {
+                speed = spd * 1.2;
+            } else {
                 speed = 0;
-                return speed;                //정지
             }
+        }
+    }
+}
+
+            }
+            
+            RobotWalk(signal, speed);
+            System.out.println("traffic light:" + signal + ", speed:" + speed);
+        }
+    }
+    public static void RobotWalk(String signal, double spd) {
+        if (signal.equals("green")) {
+        } else if (signal.equals("yellow")) {
+            speed = spd * 1.2;
+        } else {
+            speed = 0;
         }
     }
 }
