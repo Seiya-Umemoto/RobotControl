178164618e92b4fc5f3d635474bea2edd72af26b
Seiya-Umemoto <seiya.u77@gmail.com>
2020-04-22 20:07:38 +0900

    collision solved

--- Robot.java
+++ Robot.java
@@ -7,13 +7,6 @@
  */
 public class Robot
 {
-<<<<<<< HEAD
-    public double RobotWalk(String signal, double speed) {
-        if (signal.equals("green")) { // 아무 실행도 안하고 넘어감.
-        } else {
-            if (signal.equals("yellow")) {
-                speed = speed * 1.2; // 로봇의 속도를 20% 증가시킴.
-=======
     private double speed;
     private String signal;
     
@@ -36,15 +29,10 @@ public class Robot
         }else{
             if (this.signal.equals("yellow")){
                 speed = this.speed*1.2;
->>>>>>> d8bbaf913a5fcbfc8f805c4867e0431bae4e425d
             } else {
                 speed = 0;
             }
-<<<<<<< HEAD
-        }
-=======
         } 
->>>>>>> d8bbaf913a5fcbfc8f805c4867e0431bae4e425d
         return speed;
     }
 }
