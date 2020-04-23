2c28321e2aea45ca21a2f90057670d0c25c92938
lejk8104 <lejk8104@gmail.com>
2020-04-23 09:36:10 +0900

    add robotwalk_unit_test

--- RobotTest.java
+++ RobotTest.java
@@ -39,24 +39,4 @@ public class RobotTest
     public void tearDown()
     {
     }
-
-    @Test
-<<<<<<< HEAD
-    public void robot_test()
-    {
-        Robot robot1 = new Robot();
-        assertEquals(36.0, robot1.RobotWalk("yellow", 30.0), 0.1);
-=======
-    public void only_test()
-    {
-        Robot robot1 = new Robot();
-        robot1.setSpeed(30.0);
-        robot1.setSignal("green");
-        assertEquals(30.0, robot1.getSpeed(), 0);
-        assertEquals(30.0, robot1.getSpeed(), 0);
-        assertEquals("green", robot1.getSignal());
-        assertEquals(30.0, robot1.RobotWalk("green", 30.0), 0);
->>>>>>> d8bbaf913a5fcbfc8f805c4867e0431bae4e425d
-    }
 }
-
